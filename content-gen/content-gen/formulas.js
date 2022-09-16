576269c7e35baad7583a54b317c4479f521a302a
ORyan <omcentire@gmail.com>
2022-09-15 19:44:33 -1000

    Adding new hooks

---
+++ content-gen/content-gen/formulas.js
@@ -0,0 +1,33 @@
+const formulas = [{
+    name: `AIDA`,
+    description: `<b>Attention</b>: Jar the reader out of their boring ol' lives<br><b>Interest</b>: Engage their mind with unusual, counter-intuitive or fresh info<br><b>Desire</b>: Engage their heart so they want what you're offering<br><b>Action</b>: Ask them to take the next step`,
+    weight: 10
+  },
+  {
+      name: `The Credible Gap`,
+      description: `<b>Context</b>: Consider the context<br><b>Attention</b>: Grab attention<br><b>Desire: Move the reader swiftly to desire a solution to their problem<br><b>The Gap</b>: Present the consequence of failing to act<br><b>Solution</b>: Present the solution, telling only as much as is necessary to get to the next step<br><b>Action</b>: Call the prospect to act<br>Remember to always be credible in every step.`,
+      weight: 10
+  },
+  {
+      name: `FAB`,
+      description: `<b>Feature</b>: State or intro the feature
+      <b>Advantages</b>: Describe its advantages
+      <b>Benefits</b>: Describe its benefits`,
+      weight: 10
+  },
+  {
+      name: `QUEST`,
+      description: `<b>Qualify</b> the prospect<br><b>Understand</b> where they're at (show them this understanding)<br><b>Educate</b> them on a better way<br><b>Stimulate</b> them so they want that better way<br><b>Transition</b> them from where they are to becoming your customer`,
+      weight: 10
+  },
+  {
+      name: `The Four Ps`,
+      description: `<b>Picture</b>: Create a vivid scene prospects can easily put themselves in, whether about an existing pain or an aspirational future<br><b>Promise</b>: State how your solution will end that painful scene or bring that aspirational scene to life for them<br><b>Prove</b>: Support your promise! What are the consequences if you fail to do as promised? Who else has already seen you keep your promise, and how? What can you demonstrate to skeptics that your solution is easily going to ensure the promise is kept?<br><b>Push</b>: Nudge the prospect to take action`,
+      weight: 10
+  },
+  {
+      name: `PAS`,
+      description: `<b>Problem</b>: Present the problem your prospect feels<br><b>Agitation</b>: Poke at that problem until it's visceral<br><b>Solution</b>: Present your solution to the agitated problem`,
+      weight: 10
+  }
+]
