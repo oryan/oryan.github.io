11ba194eb324071e405ca8c094d919971e427a9b
ORyan <omcentire@gmail.com>
2022-11-22 00:18:30 -1000

    Episode Gen
    Adding Episode gen to the repo

---
+++ episode-gen/plots.js
@@ -0,0 +1,101 @@
+const plots = [{
+    name: "Quest",
+    description: "The hero searches for something, someone, or somewhere. In reality, they may be searching for themselves, with the outer journey mirrored internally. They may be joined by a companion, who takes care of minor detail and whose limitations contrast with the hero's greater qualities.",
+    weight: 10
+  },
+  {
+    name: "Adventure",
+    description: "The protagonist goes on an adventure, much like a quest, but with less of a focus on the end goal or the personal development of hero hero. In the adventure, there is more action for action's sake.",
+    weight: 10
+  },
+  {
+    name: "Pursuit",
+    description: "In this plot, the focus is on chase, with one person chasing another (and perhaps with multiple and alternating chase). The pursued person may be often cornered and somehow escape, so that the pursuit can continue. Depending on the story, the pursued person may be caught or may escape.",
+    weight: 10
+  },
+  {
+    name: "Rescue",
+    description: "In the rescue, somebody is captured, who must be released by the hero or heroic party. A triangle may form between the protagonist, the antagonist and the victim. There may be a grand duel between the protagonist and antagonist, after which the victim is freed.",
+    weight: 10
+  },
+  {
+    name: "Escape",
+    description: "In a kind of reversal of the rescue, a person must escape, perhaps with little help from others. In this, there may well be elements of capture and unjust imprisonment. There may also be a pursuit after the escape.",
+    weight: 10
+  },
+  {
+    name: "Revenge",
+    description: "In the revenge plot, a wronged person seeks retribution against the person or organization which has betrayed or otherwise harmed them or loved ones, physically or emotionally. This plot depends on moral outrage for gaining sympathy from the audience.",
+    weight: 10
+  },
+  {
+    name: "The Riddle",
+    description: "The riddle plot entertains the audience and challenges them to find the solution before the hero, who steadily and carefully uncovers clues and hence the final solution. The story may also be spiced up with terrible consequences if the riddle is not solved in time.",
+    weight: 10
+  },
+  {
+    name: "Rivalry",
+    description: "In rivalry, two people or groups are set as competitors that may be good hearted or as bitter enemies. Rivals often face a zero-sum game, in which there can only be one winner, for example where they compete for a scarce resource or the heart of a single other person.",
+    weight: 10
+  },
+  {
+    name: "Underdog",
+    description: "The underdog plot is similar to rivalry, but where one person (usually the hero) has less advantage and might normally be expected to lose. The underdog usually wins through greater tenacity and determination (and perhaps with the help of friendly others).",
+    weight: 10
+  },
+  {
+    name: "Temptation",
+    description: "In the temptation plot, a person is tempted by something that, if taken, would somehow diminish them, often morally. Their battle is thus internal, fighting against their inner voices which tell them to succumb.",
+    weight: 6
+  },
+  {
+    name: "Metamorphosis",
+    description: "In this fantastic plot, the protagonist is physically transformed, perhaps into beast or perhaps into some spiritual or alien form. The story may then continue with the changed person struggling to be released or to use their new form for some particular purpose. Eventually, the hero is released, perhaps through some great act of love.",
+    weight: 10
+  },
+  {
+    name: "Transformation",
+    description: "The transformation plot leads to change of a person in some way, often driven by unexpected circumstance or event. After setbacks, the person learns and usually becomes something better.",
+    weight: 8
+  },
+  {
+    name: "Maturation",
+    description: "The maturation plot is a special form of transformation, in which a person grows up. The veils of younger times are lost as they learn and grow. Thus the rudderless youth finds meaning or perhaps an older person re-finds their purpose.",
+    weight: 8
+  },
+  {
+    name: "Love",
+    description: "The love story is a perennial tale of lovers finding one another, perhaps through a background of danger and woe. Along the way, they become separated in some way, but eventually come together in a final joyous reunion.",
+    weight: 2
+  },
+  {
+    name: "Forbidden Love",
+    description: "The story of forbidden love happens when lovers are breaking some social rules, such as in an adulterous relationship or worse. The story may thus turn around their inner conflicts and the effects of others discovering their tryst.",
+    weight: 1
+  },
+  {
+    name: "Sacrifice",
+    description: "In sacrifice, the nobler elements of the human sprit are extolled as someone gives much more than most people would give. The person may not start with the intent of personal sacrifice and may thus be an unintentional hero, thus emphasizing the heroic nature of the choice and act.",
+    weight: 7
+  },
+  {
+    name: "Discovery",
+    description: "The discovery plot is strongly focused on the character of the hero who discovers something great or terrible and hence must make a difficult choice. The importance of the discovery might not be known at first and the process of revelation be important to the story.",
+    weight: 8
+  },
+  {
+    name: "Wretched Excess",
+    description: "In stories of wretched excess, the protagonist goes beyond normally accepted behavior as the world looks on, horrified, perhaps in realization that 'there before the grace of God go I' and that the veneer of civilization is indeed thin.",
+    weight: 2
+  },
+  {
+    name: "Ascension",
+    description: "In the ascension plot, the protagonist starts in the virtual gutter, as a sinner of some kind. The plot then shows their ascension to becoming a better person, often in response to stress that would defeat a normal person. Thus they achieve deserved heroic status.",
+    weight: 7
+  },
+  {
+    name: "Descension",
+    description: "In the opposite to ascension, a person of initially high standing descends to the gutter and moral turpitude, perhaps sympathetically as they are unable to handle stress and perhaps just giving in to baser vices.",
+    weight: 2
+  }
+]
